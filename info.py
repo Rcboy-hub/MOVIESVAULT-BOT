289b1107daee4e4dc5c23dd837ba26eedfa486e9
Rcboy-hub <harishentreprenur@gmail.com>
2025-01-11 08:29:03 -0800

    Update info.py

--- info.py
+++ info.py
@@ -45,7 +45,7 @@ reqst_channel = environ.get('REQST_CHANNEL_ID', '')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 
 # This Is Your Bot Support Group Id , Here Bot Will Not Give File Because This Is Support Group.
-support_chat_id = environ.get('SUPPORT_CHAT_ID', '')
+support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002267456913')
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 
 # This Channel Is For Index Request 
@@ -88,9 +88,9 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/moviesvaultgroup')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/moviesscomgroup')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/mediaagalaxy')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'moviesscomgroup') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'moviesvaultgroup') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/mediaagalaxy')
 
 # True Or False
