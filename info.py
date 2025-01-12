371a70ead9c9efd353c4216dafea65b9abde0d2b
Rcboy-hub <harishentreprenur@gmail.com>
2025-01-11 21:06:59 -0800

    Update info.py

--- info.py
+++ info.py
@@ -125,7 +125,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'https://tnshort.net/')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'bbfd6f56eb3b64c1bcceea0b147e1440988aa9e6')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/hdmovies2792/9') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
