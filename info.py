6e841737143b6f498f24aa3abfb33391c6e038d5
Rcboy-hub <harishentreprenur@gmail.com>
2025-01-20 06:07:56 -0800

    Update info.py

--- info.py
+++ info.py
@@ -125,10 +125,10 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '350d6053097c
 
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'https://modijiurl.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', '350d6053097c06f2fff4b81d203e4d2cc3a5aefa')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/hdmovies2792/10') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'http://short2url.in/http://short2url.in/')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'caac0ebeca0f88d106996b399a993c6e9bcc534a')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/hdmovies2792/11') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
 # Others
